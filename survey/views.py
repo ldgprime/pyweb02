b1374fcfea9998f0824a767c40e0e637d4b4945a
ldgprime <leehearts@naver.com>
2020-03-27 16:30:15 +0900

    pyweb02

---
+++ survey/views.py
@@ -0,0 +1,34 @@
+from django.shortcuts import render
+from survey.models import Survey, Answer
+from anaconda_navigator.utils.py3compat import request
+
+# Create your views here.
+def main(request):
+    survey=Survey.objects.filter(status='y').order_by('-survey_idx')[0]
+    
+    return render(request, 'main.html',{'survey':survey})
+
+def save_survey(request):
+    dto=Answer(survey_idx=request.POST['survey_idx'], num=request.POST['num'])
+    dto.save()
+    return render(request, 'success.html')
+
+def show_result(request):
+    idx=request.GET['survey_idx']
+    ans=Survey.objects.get(survey_idx=idx)
+    answer=[ans.ans1, ans.ans2, ans.ans3, ans.ans4]
+    surveyList=Survey.objects.raw("""
+    select
+    survey_idx,num,count(num) sum_num,
+    round((select count(*) from survey_answer
+            where survey_idx=a.survey_idx and num=a.num)*100.0
+         (select count(*) from survey_answer
+            where survey_idx=a.survey_idx),1) rate
+    from survey_answer a
+    where survey_idx=%s
+    group by survey_idx,num
+    order by num    
+    """,idx)
+    surveyList=zip(surveyList,answer)
+    print('surveyList:',surveyList)
+    print('answer:',answer)
